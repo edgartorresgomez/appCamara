e5dbb04558da3b745859b0ea92c27109737ebb2f
edgar.torres <edgartorresgomez@hotmail.com>
2018-03-23 00:41:51 -0500

    resolve conflict
    Signed-off-by: edgar.torres <edgartorresgomez@hotmail.com>

---
+++ src/pages/home/home.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  private base64Image : string = "";
+  constructor(public navCtrl: NavController, private camera: Camera) {
+    
+  }
+  tomaFoto(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    },(err) => {
+      // Handle error
+    });
+  }
+  //this.img = document.getElementById("img-display");
+  //this.img.style.display="none";
+}
